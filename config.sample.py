10f267ed6fbbba1c7ca4e35778ae32717e84c8dc
CrocHold <yash_g@me.iitr.ac.in>
2024-02-05 04:17:37 +0530

    samples

---
+++ config.sample.py
@@ -0,0 +1,10 @@
+# Update api keys
+YOUTUBE_API_KEYS = [
+    'api-key1',
+    'api-key2',
+    'api-key3'
+                    ]
+
+
+# Update if required, keeping the db name "assignment"
+MONGO_URI = 'mongodb://localhost:27017/assignment'
